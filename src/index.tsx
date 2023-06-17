d649047a6022313a4856b8eaf4d5b613a814b9d6
otonielemanuel <otonielemanuel21@gmail.com>
2023-06-17 22:48:51 +0100

    first commit: lib

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+const title = {
+  fontFamily: 'sans-serif',
+  fontSize: '2rem',
+  color: '#f5f5f5'
+}
+
+const text = {
+  fontFamily: 'sans-serif',
+  fontSize: '14px',
+  color: '#eeeeee'
+}
+
+// Delete me
+export const Otoniel = () => {
+  return (
+    <div>
+      <h1 style={title}>Otoniel Emanuel</h1>
+      <br />
+      <p style={text}>Aspiring front-end software developer.🤓</p>
+    </div>
+  )
+};
